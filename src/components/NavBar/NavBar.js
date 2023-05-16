c10d922c7d1961e597ce10b64e297f767575d19d
carlosm22700 <carlosmartinez.steven@gmail.com>
2023-05-16 15:24:23 -0700

    updates NavBar styling, updates favicon

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,32 +1,49 @@
-import { Link } from "react-router-dom";
+import { Link as RouterLink } from "react-router-dom";
 import * as userService from "../../utilities/users-service";
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import IconButton from "@mui/material/IconButton";
 import Button from "@mui/material/Button";
+import Box from "@mui/material/Box";
+import Avatar from "@mui/material/Avatar";
 
 export default function NavBar({ user, setUser }) {
   function handleLogOut() {
-    // Delegate to the users-service
     userService.logOut();
-    // Update state will also cause a re-render
     setUser(null);
   }
 
   return (
-    <nav>
-      <Link to="/cart">
-        <Button>
-          <ShoppingCartIcon /> Cart
+    <AppBar position="static">
+      <Toolbar>
+        <Button
+          color="inherit"
+          component={RouterLink}
+          to="/orders/shop"
+          sx={{ marginRight: 2 }}
+        >
+          <Avatar alt="Logo" src="/TechMart-logos_transparent.png" />
         </Button>
-      </Link>
-      {/* <Link to="/orders">Order History</Link> */}
-      &nbsp; | &nbsp;
-      <Link to="/orders/shop">Shop</Link>
-      &nbsp; | &nbsp;
-      {user && <span>&nbsp;Welcome, {user.name}</span>}
-      &nbsp; | &nbsp;
-      <Link to="" onClick={handleLogOut}>
-        Log Out
-      </Link>
-    </nav>
+        <IconButton
+          edge="start"
+          color="inherit"
+          aria-label="cart"
+          component={RouterLink}
+          to="/cart"
+        >
+          <ShoppingCartIcon />
+        </IconButton>
+        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          Welcome, {user ? user.name : ""}
+        </Typography>
+        <Box sx={{ display: "flex", alignItems: "center" }}>
+          <Button color="inherit" onClick={handleLogOut}>
+            Log Out
+          </Button>
+        </Box>
+      </Toolbar>
+    </AppBar>
   );
 }
