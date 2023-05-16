310124d678b0d151ee4d47ddd7df625d147ed08d
carlosm22700 <carlosmartinez.steven@gmail.com>
2023-05-16 12:29:49 -0700

    adds CssBaseline from MUI

--- src/index.js
+++ src/index.js
@@ -7,10 +7,12 @@ import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
+import CssBaseline from "@mui/material/CssBaseline";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
+    <CssBaseline />
     <Router>
       <App />
     </Router>
