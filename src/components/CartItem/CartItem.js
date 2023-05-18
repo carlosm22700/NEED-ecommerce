3f2859f3e386c7ff14c10ed47f32191c9193cc25
carlosm22700 <carlosmartinez.steven@gmail.com>
2023-05-17 17:12:11 -0700

    renders product img on cart page

--- src/components/CartItem/CartItem.js
+++ src/components/CartItem/CartItem.js
@@ -1,96 +1,3 @@
-// // import {
-// //   Card,
-// //   CardContent,
-// //   Typography,
-// //   CardActions,
-// //   Button,
-// //   TextField,
-// // } from "@mui/material";
-
-// // function CartItem({ product, removeFromCart, updateQuantity }) {
-// //   const handleQuantityChange = (event) => {
-// //     const newQuantity = Number(event.target.value);
-
-// //     if (newQuantity > 0) {
-// //       updateQuantity(product.id, newQuantity);
-// //     }
-// //   };
-
-// //   return (
-// //     <Card variant="outlined" sx={{ marginTop: 2 }}>
-// //       <CardContent>
-// //         <Typography variant="h6">{product.title}</Typography>
-// //         <Typography color="text.secondary">
-// //           ${product.price} x
-// //           <TextField
-// //             type="number"
-// //             value={product.quantity}
-// //             onChange={handleQuantityChange}
-// //             InputProps={{ inputProps: { min: 1 } }}
-// //           />
-// //         </Typography>
-// //       </CardContent>
-// //       <CardActions>
-// //         <Button size="small" onClick={() => removeFromCart(product.id)}>
-// //           Remove
-// //         </Button>
-// //       </CardActions>
-// //     </Card>
-// //   );
-// // }
-
-// // export default CartItem;
-// // CartItem.js
-// import {
-//   Card,
-//   CardContent,
-//   Typography,
-//   CardActions,
-//   Button,
-//   TextField,
-//   Box,
-// } from "@mui/material";
-
-// function CartItem({ product, removeFromCart, updateQuantity }) {
-//   const handleQuantityChange = (event) => {
-//     const newQuantity = Number(event.target.value);
-
-//     if (newQuantity > 0) {
-//       updateQuantity(product.id, newQuantity);
-//     }
-//   };
-
-//   return (
-//     <Card variant="outlined" sx={{ marginTop: 2, padding: 2 }}>
-//       <CardContent>
-//         <Typography variant="h6" align="center" fontWeight="bold">
-//           {product.title}
-//         </Typography>
-//         <Box display="flex" justifyContent="space-between" alignItems="center">
-//           <Typography variant="body1">${product.price.toFixed(2)}</Typography>
-//           <TextField
-//             type="number"
-//             value={product.quantity}
-//             onChange={handleQuantityChange}
-//             InputProps={{
-//               inputProps: { min: 1 },
-//               sx: { width: 50 },
-//             }}
-//           />
-//         </Box>
-//       </CardContent>
-//       <CardActions>
-//         <Button size="small" onClick={() => removeFromCart(product.id)}>
-//           Remove
-//         </Button>
-//       </CardActions>
-//     </Card>
-//   );
-// }
-
-// export default CartItem;
-
-// CartItem.js
 import {
   Card,
   CardContent,
@@ -98,7 +5,8 @@ import {
   CardActions,
   Button,
   TextField,
-  Box,
+  CardMedia,
+  Grid,
 } from "@mui/material";
 
 function CartItem({ product, removeFromCart, updateQuantity }) {
@@ -119,26 +27,34 @@ function CartItem({ product, removeFromCart, updateQuantity }) {
         <Typography variant="h6" align="center" fontWeight="bold">
           {product.title}
         </Typography>
-        <Box
-          display="flex"
-          justifyContent="space-between"
-          alignItems="center"
-          marginTop={2}
-        >
-          <Typography variant="h6" color="primary">
-            ${product.price.toFixed(2)}
-          </Typography>
-          <TextField
-            type="number"
-            value={product.quantity}
-            onChange={handleQuantityChange}
-            InputProps={{
-              inputProps: { min: 1 },
-              sx: { width: 50, marginLeft: 2 },
-            }}
-          />
-        </Box>
+        <Grid container alignItems="center" spacing={2}>
+          <Grid item xs={4} sm={4}>
+            <Typography variant="h6" color="primary">
+              ${product.price.toFixed(2)}
+            </Typography>
+          </Grid>
+          <Grid item xs={4} sm={4} container justifyContent="center">
+            <CardMedia
+              component="img"
+              image={product.thumbnail}
+              alt={product.title}
+              sx={{ width: 100, height: 100 }} // adjust as needed
+            />
+          </Grid>
+          <Grid item xs={4} sm={4} container justifyContent="flex-end">
+            <TextField
+              type="number"
+              value={product.quantity}
+              onChange={handleQuantityChange}
+              InputProps={{
+                inputProps: { min: 1 },
+                sx: { width: 50 },
+              }}
+            />
+          </Grid>
+        </Grid>
       </CardContent>
+
       <CardActions>
         <Button size="small" onClick={() => removeFromCart(product.id)}>
           Remove
--- src/pages/NewOrderPage/NewOrderPage.js
+++ src/pages/NewOrderPage/NewOrderPage.js
@@ -1,135 +1,3 @@
-// import { useEffect, useState } from "react";
-// import { getProducts, getCategories } from "../../utilities/products-service";
-// import ProductCard from "../../components/ProductCard/ProductCard";
-// import SearchIcon from "@mui/icons-material/Search";
-// import {
-//   Grid,
-//   Container,
-//   Button,
-//   FormControl,
-//   InputLabel,
-//   Select,
-//   MenuItem,
-//   TextField,
-//   InputAdornment,
-//   Box,
-// } from "@mui/material";
-
-// function NewOrderPage({ addToCart }) {
-//   const [products, setProducts] = useState([]);
-//   const [categories, setCategories] = useState([]);
-//   const [selectedCategory, setSelectedCategory] = useState("");
-//   const [skip, setSkip] = useState(0);
-//   const [search, setSearch] = useState("");
-
-//   useEffect(() => {
-//     async function fetchData() {
-//       const fetchedCategories = await getCategories();
-//       setCategories(fetchedCategories);
-//     }
-
-//     fetchData();
-//   }, []);
-
-//   useEffect(() => {
-//     fetchData();
-//   }, [selectedCategory, skip]);
-
-//   async function fetchData() {
-//     const fetchedProducts = await getProducts(
-//       selectedCategory,
-//       4,
-//       skip,
-//       search
-//     );
-//     if (skip === 0) {
-//       setProducts(fetchedProducts.products);
-//     } else {
-//       setProducts((prevProducts) => [
-//         ...prevProducts,
-//         ...fetchedProducts.products,
-//       ]);
-//     }
-//   }
-
-//   const handleLoadMore = () => {
-//     setSkip((prevSkip) => prevSkip + 4);
-//   };
-
-//   const handleCategoryChange = (event) => {
-//     setSelectedCategory(event.target.value);
-//     setSkip(0);
-//   };
-
-//   const handleSearchChange = (event) => {
-//     setSearch(event.target.value);
-//   };
-
-//   return (
-//     <Container maxWidth="lg" style={{ marginTop: "20px" }}>
-//       <Box
-//         sx={{ display: "flex", justifyContent: "center", marginBottom: "20px" }}
-//       >
-//         <TextField
-//           label="Search"
-//           variant="outlined"
-//           size="small"
-//           sx={{ marginRight: 2 }}
-//           value={search}
-//           onChange={handleSearchChange}
-//           InputProps={{
-//             startAdornment: (
-//               <InputAdornment position="start">
-//                 <SearchIcon />
-//               </InputAdornment>
-//             ),
-//           }}
-//         />
-//         <FormControl variant="outlined">
-//           <InputLabel id="category-label">Categories</InputLabel>
-//           <Select
-//             labelId="category-label"
-//             id="category-select"
-//             value={selectedCategory}
-//             onChange={handleCategoryChange}
-//             label="Category"
-//           >
-//             <MenuItem value="">
-//               <em>None</em>
-//             </MenuItem>
-//             {categories.map((category, index) => (
-//               <MenuItem value={category} key={index}>
-//                 {category}
-//               </MenuItem>
-//             ))}
-//           </Select>
-//         </FormControl>
-//       </Box>
-//       <Grid
-//         container
-//         spacing={3}
-//         justifyContent="space-around"
-//         alignItems="center"
-//       >
-//         {products.map((product) => (
-//           <Grid item xs={12} sm={6} md={4} key={product.id}>
-//             <ProductCard product={product} addToCart={addToCart} />
-//           </Grid>
-//         ))}
-//       </Grid>
-//       <Box
-//         sx={{ display: "flex", justifyContent: "center", marginTop: "20px" }}
-//       >
-//         <Button variant="contained" color="primary" onClick={handleLoadMore}>
-//           Load More
-//         </Button>
-//       </Box>
-//     </Container>
-//   );
-// }
-
-// export default NewOrderPage;
-
 import React, { useEffect, useState } from "react";
 import { getProducts, getCategories } from "../../utilities/products-service";
 import ProductCard from "../../components/ProductCard/ProductCard";
@@ -242,7 +110,12 @@ function NewOrderPage({ addToCart }) {
         ))}
       </Grid>
       <Box
-        sx={{ display: "flex", justifyContent: "center", marginTop: "20px" }}
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          marginTop: "20px",
+          paddingBottom: "25px",
+        }}
       >
         <Button variant="contained" onClick={handleLoadMore}>
           Load More
